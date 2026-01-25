e7ea66050cd66d1e182e47a2b559ff6099311e16
guwno-2137 <113305838+guwno-2137@users.noreply.github.com>
2026-01-25 17:46:22 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,36 +1,44 @@
+
 import Link from "next/link";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [open, setOpen] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-inner">
 
-        {/* LOGO + NAZWA */}
+        {/* BRAND */}
         <Link href="/" legacyBehavior>
           <a className="navbar-brand">
-            <img
-              src="/logo.png"
-              alt="Roman Zupa"
-              className="navbar-logo"
-            />
-            <span className="navbar-title">
-              Roman Zupa
-            </span>
+            <img src="/logo.png" alt="Roman Zupa" className="navbar-logo" />
+            <span className="navbar-title">Roman Zupa</span>
           </a>
         </Link>
 
+        {/* BURGER */}
+        <button
+          className="navbar-burger"
+          onClick={() => setOpen(!open)}
+        >
+          ☰
+        </button>
+
         {/* MENU */}
-        <div className="navbar-menu">
+        <div className={`navbar-menu ${open ? "open" : ""}`}>
           <Link href="/shop" legacyBehavior>
-            <a>Kolekcja</a>
+            <a onClick={() => setOpen(false)}>Kolekcja</a>
           </Link>
 
           <Link href="/cart" legacyBehavior>
-            <a>Koszyk</a>
+            <a onClick={() => setOpen(false)}>Koszyk</a>
           </Link>
 
           <Link href="/admin" legacyBehavior>
-            <a className="navbar-admin">Panel</a>
+            <a className="navbar-admin" onClick={() => setOpen(false)}>
+              Panel
+            </a>
           </Link>
         </div>
 
