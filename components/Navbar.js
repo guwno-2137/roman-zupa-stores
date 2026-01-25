25eca957097d09ce76eb8296667cd47520d0842f
guwno-2137 <113305838+guwno-2137@users.noreply.github.com>
2026-01-25 17:12:25 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,36 +6,35 @@ export default function Navbar() {
       <div className="flex justify-between items-center px-10 md:px-24 py-6">
 
         {/* LOGO */}
- <Link href="/">
-  <div className="flex items-center gap-3 cursor-pointer">
-    <img
-      src="/Public/Untitled5_20260120220021.png"
-      alt="Roman Zupa"
-      className="h-8 md:h-10 object-contain"
-    />
-  </div>
-</Link>
-
+        <Link href="/" legacyBehavior>
+          <a className="flex items-center gap-3 cursor-pointer">
+            <img
+              src="/Public/Untitled5_20260120220021.png"
+              alt="Roman Zupa"
+              className="h-8 md:h-10 object-contain"
+            />
+          </a>
+        </Link>
 
         {/* MENU */}
         <div className="flex items-center space-x-10 text-sm tracking-widest uppercase">
 
-          <Link href="/shop">
-            <span className="hover:text-gold transition cursor-pointer">
+          <Link href="/shop" legacyBehavior>
+            <a className="hover:text-gold transition">
               Kolekcja
-            </span>
+            </a>
           </Link>
 
-          <Link href="/cart">
-            <span className="hover:text-gold transition cursor-pointer">
+          <Link href="/cart" legacyBehavior>
+            <a className="hover:text-gold transition">
               Koszyk
-            </span>
+            </a>
           </Link>
 
-          <Link href="/admin">
-            <span className="border border-gold px-4 py-2 hover:bg-gold hover:text-black transition cursor-pointer">
+          <Link href="/admin" legacyBehavior>
+            <a className="border border-gold px-4 py-2 hover:bg-gold hover:text-black transition">
               Panel
-            </span>
+            </a>
           </Link>
 
         </div>
