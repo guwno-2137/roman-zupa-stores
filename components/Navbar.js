09d4a2e0241b327809253cbd5f3b433826a39566
guwno-2137 <113305838+guwno-2137@users.noreply.github.com>
2026-01-25 17:01:32 +0100

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,11 +6,16 @@ export default function Navbar() {
       <div className="flex justify-between items-center px-10 md:px-24 py-6">
 
         {/* LOGO */}
-        <Link href="/">
-          <span className="text-xl font-bold tracking-wide">
-            ROMAN <span className="text-gold">ZUPA</span>
-          </span>
-        </Link>
+ <Link href="/">
+  <div className="flex items-center gap-3 cursor-pointer">
+    <img
+      src="/Public/Untitled5_20260120220021.png"
+      alt="Roman Zupa"
+      className="h-8 md:h-10 object-contain"
+    />
+  </div>
+</Link>
+
 
         {/* MENU */}
         <div className="flex items-center space-x-10 text-sm tracking-widest uppercase">
